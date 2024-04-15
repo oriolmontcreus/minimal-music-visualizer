748e9184c87dc06433ea4b6d14c93b32120613fa
oriolmontcreus <licensedgamer641319@gmail.com>
2024-04-15 17:32:52 +0200

    feat: added comments and fix: createGradientCanvas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'https://threejs.org/build/three.module.js';
 import * as dat from 'https://cdn.jsdelivr.net/npm/dat.gui@0.7.7/build/dat.gui.module.js';
 
+//Minified three.js setup ----------
 let audioContext, analyser, audioElement = new Audio('./audio1.mp3'), source, currentTimeController;
 const scene = new THREE.Scene(), camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000), renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true }), gui = new dat.GUI({ autoPlace: false }), audioControls = { play: startAudio, pause: () => audioElement.paused || audioElement.pause(), currentTime: 0, audioUrl: '' }, texture = new THREE.CanvasTexture(createGradientCanvas()), torus = new THREE.Mesh(new THREE.TorusGeometry(1, 0.2, 16, 100), new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide, transparent: true, opacity: 0.9 })), statsElement = document.createElement('div'), audioMenu = createAudioMenu();
 
@@ -23,7 +24,9 @@ document.addEventListener('dragover', event => event.preventDefault());
 document.addEventListener('drop', handleDropEvent);
 attachAudioEventListeners();
 animate();
+//End of minified three.js setup ----------
 
+//Starts audio playback and initializes the audio context if not already done
 function startAudio() {
   if (!audioContext) {
     audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -35,23 +38,32 @@ function startAudio() {
   if (audioElement.paused) audioElement.play();
 }
 
+//Continuously updates the scene and renders it
 function animate() {
   requestAnimationFrame(animate);
   updateTorus();
   renderer.render(scene, camera);
 }
 
+// Creates a canvas with a radial gradient from blue to dark blue
 function createGradientCanvas() {
-  const canvas = document.createElement('canvas'), context = canvas.getContext('2d'), gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+  const canvas = document.createElement('canvas');
   canvas.width = 2048;
   canvas.height = 2048;
+
+  const context = canvas.getContext('2d');
+  const gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+
   gradient.addColorStop(0.5, 'blue');
   gradient.addColorStop(1, 'darkblue');
+
   context.fillStyle = gradient;
   context.fillRect(0, 0, canvas.width, canvas.height);
+
   return canvas;
 }
 
+//Updates scale/opacity of the torus based on the bass frequency of the audio
 function updateTorus() {
   if (analyser) {
     const data = new Uint8Array(analyser.frequencyBinCount);
@@ -63,6 +75,7 @@ function updateTorus() {
   }
 }
 
+//Creates a dropdown menu for selecting audio files
 function createAudioMenu() {
   const audioMenu = document.createElement('select');
   audioMenu.style = 'position: absolute; top: 10px; left: 500px;';
@@ -70,6 +83,7 @@ function createAudioMenu() {
   return audioMenu;
 }
 
+//Fetches the provided audio files and adds them as options to the audio menu (the audios hardcoded in the repo, so audio1.mp3 and audio2.mp3)
 function loadAudioFiles(files, audioMenu) {
   files.forEach(file => fetch(file).then(response => response.blob()).then(blob => {
     const url = URL.createObjectURL(blob), option = document.createElement('option');
@@ -79,6 +93,7 @@ function loadAudioFiles(files, audioMenu) {
   }));
 }
 
+//Attaches event listeners to the audio element to update the current time controller
 function attachAudioEventListeners() {
   audioElement.addEventListener('loadedmetadata', () => currentTimeController.max(audioElement.duration));
   audioElement.addEventListener('timeupdate', () => {
@@ -87,6 +102,7 @@ function attachAudioEventListeners() {
   });
 }
 
+//Logic to handle the event when a different audio file is selected from the menu
 function handleAudioMenuChange(event) {
   if (audioContext) audioContext.close();
   audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -101,6 +117,7 @@ function handleAudioMenuChange(event) {
   currentTimeController.updateDisplay();
 }
 
+//Logic to handle the event when an audio file is dropped onto the page
 async function handleDropEvent(event) {
   event.preventDefault();
   if (audioContext) audioContext.close();
